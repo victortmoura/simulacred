e8849cb51809021d00ba19c59ab5c128fbee7d7c
Victor Moura <victorttmoura@gmail.com>
2025-01-25 23:43:39 -0300

    feat: add initial project structure

---
+++ src/main/java/com/br/simulacred/SimulacredApplication.java
@@ -0,0 +1,13 @@
+package com.br.simulacred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulacredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulacredApplication.class, args);
+	}
+
+}
